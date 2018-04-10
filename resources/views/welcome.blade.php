75b78b9ae21640dbe93b066edee492dbaa3dac9e
BenjaminDardy <benjamin.dardy@viacesi.fr>
2018-04-10 17:14:39 +0200

    Footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 </head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="../public/css/styleAcceuil.css">
+<link rel="stylesheet" href="../public/css/styleAccueil.css">
 <body>
 
 <header>
