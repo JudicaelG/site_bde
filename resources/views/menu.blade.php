35360e31e9e1bb2c89ceec60f6459cf0afd1bed8
AymericLemoine <aymeric.lemoine@viacesi.fr>
2018-04-10 17:01:41 +0200

    Ajout de la page d'acceuil, du menu et du footer. Ainsi que la route pour y acceder

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,15 @@
+<nav id="menu">
+
+    <div class="element_menu">
+
+        <ul>
+            <li><a href="welcome"> Acceuil </a></li>
+            <li><a href="boutique"> Boutique </a></li>
+            <li><a href="evenement.php"> Evenements </a></li>
+            <li><a href="boite_idee.blade.php"> Boite à idées </a></li>
+
+        </ul>
+
+    </div>
+
+</nav>
