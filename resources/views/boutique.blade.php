bbc2ebc05d2707b292638f41b1e911378e90c71c
BenjaminDardy <benjamin.dardy@viacesi.fr>
2018-04-10 17:07:35 +0200

    Inclusion du menu via @include

--- resources/views/boutique.blade.php
+++ resources/views/boutique.blade.php
@@ -16,16 +16,7 @@
         <p id="titreFondBoutique">Boutique BDE</p>
     </div>
 
-    <nav>
-        <div class="element_menu">
-            <ul>
-                <li><a href="welcome"> Acceuil </a></li>
-                <li><a href="boutique"> Boutique </a></li>
-                <li><a href="evenement"> Evenements </a></li>
-                <li><a href="boite_idee"> Boite à idées </a></li>
-            </ul>
-        </div>
-    </nav>
+    @include('menu')
 
     <div id="meilleuresVentes">
         <p>Meilleures Ventes</p>
--- resources/views/testcommit.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: bdard
- * Date: 10/04/2018
- * Time: 11:16
- */
