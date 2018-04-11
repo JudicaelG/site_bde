4a183800d9c23bdbff672a7fa4815e33a7e2dc7d
AymericLemoine <aymeric.lemoine@viacesi.fr>
2018-04-11 11:45:45 +0200

    Ajout boite idée

--- resources/views/page/boiteAidee.blade.php
+++ resources/views/page/boiteAidee.blade.php
@@ -10,6 +10,28 @@
 
 @section ('content')
 
+<div id="corps">
+
+
+    <div id="encre_idee">
+        <a href="#ajout_idee"> Ajouter une idée</a>
+    </div>
+    <div id="idee">
+        <? //include("displayIdee");?>
+    </div>
+
+    <div id="ajout_idee">
+        <h3>Boite à idée</h3>
+        <form action="">
+            <label class="titre">Titre: </label><input class="tireInput" type="text" name="titreIdee"><br>
+            <label class="description">Description: </label><textarea name="descriptionIdee" cols="30" rows="10" value="Ajouter une description à votre événement"></textarea>
+            <input type="submit" value="Valider">
+
+        </form>
+    </div>
+
+</div>
+
 
 
 @endsection
