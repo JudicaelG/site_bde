efb88bceca59a53588c932bfb454ced3b31a85de
BenjaminDardy <benjamin.dardy@viacesi.fr>
2018-04-12 16:59:06 +0200

    Fonctionnalite Boite a idee manque que le statut

--- resources/views/boiteidee/show.blade.php
+++ resources/views/boiteidee/show.blade.php
@@ -12,11 +12,16 @@
     <h1> {{ $idee->titre }}</h1>
 
     <a href="/boiteidee/<?php echo $idee->id;?>/edit"> <button type="button" class="btn btn-primary" >Edit</button></a>
+    
+    <br>
+
     <form action="{{url('boiteidee', [$idee->id])}}" method="POST">
         <input type="hidden" name="_method" value="DELETE">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <input type="submit" class="btn btn-danger" value="Delete"/>
     </form>
+
+
 
     <div class="jumbotron text-center">
         <p>
