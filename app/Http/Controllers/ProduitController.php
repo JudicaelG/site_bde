e3cf2d79d3d4c0bdc85842da6aedf6abe59373b5
Judicaël <judicael.guillaume@viacesi.fr>
2018-04-18 08:36:58 +0200

    Les meilleurs ventes sont presque là

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -13,9 +13,22 @@ class ProduitController extends Controller
     {
 		$produits = DB::table('produits')
 					->join('images', 'produits.id', '=', 'images.id_produit')
+					->join('categorie_produits', 'produits.id_categorie', '=', 'categorie_produits.id')
 					->get();
+		$meilleurventes = DB::table('contient_produits')
+							->select(DB::raw('COUNT(contient_produits.id_produit) as nombre'))
+							->join('produits', 'contient_produits.id_produit', '=', 'produits.id')
+							->join('images', 'produits.id', '=', "images.id_produit")
+							->where('produits.id', '=', 'contient_produits.id_produit')
+							->groupby('contient_produits.id_produit')
+							->get();
+		
         return view('boutique.index')
-				->with(compact('produits', $produits));
+				->with(compact('produits', $produits))
+				->with(compact('meilleurventes', $meilleurventes));
+		
+		
+			
     }
 	
 	public function show($id)
--- resources/views/boutique/index.blade.php
+++ resources/views/boutique/index.blade.php
@@ -13,9 +13,12 @@
     <div id="meilleuresVentes">
         <p>Meilleures Ventes</p>
         <div class="imgMeilleuresVentes">
-            <div class="produits"><img src="http://vision.gel.ulaval.ca/~jflalonde/cours/4105/h15/tps/results/tp2/SEBEL83/images/lena.jpg"></div>
-            <div class="produits"><img src="http://vision.gel.ulaval.ca/~jflalonde/cours/4105/h15/tps/results/tp2/SEBEL83/images/lamb.jpg"></div>
-            <div class="produits"><img src="http://vision.gel.ulaval.ca/~jflalonde/cours/4105/h15/tps/results/tp2/SEBEL83/images/koopa.jpg"></div>
+			@foreach($meilleurventes as $meilleurvente)
+            <div class="produits">
+				<h4>{{ $meilleurvente->titre }}</h4>
+				<img src="{{$meilleurvente->lien}}" alt="{{ $meilleurvente->alt}}">
+			</div>
+			@endforeach
         </div>
     </div>
 
@@ -25,6 +28,7 @@
 			@foreach($produits as $produit)
             <div class="produits">
 				<h2 class="blog-post"><a href="/boutique/<?php echo $produit->id?>">{{ $produit->titre }}</a></h2>
+				<h5>{{ $produit->nom }}</h5>
 				<h4>{{ $produit->description }}</h4>
 				<img src="{{$produit->lien}}" alt="{{ $produit->alt}}">
 				<h4>Prix :{{ $produit->prix }} €</h4>
