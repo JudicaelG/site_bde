d330f449ec00a18bc871f4fd2ccc418f625e7869
Judicaël <judicael.guillaume@viacesi.fr>
2018-04-19 08:44:14 +0200

    correction navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -13,7 +13,7 @@
         <li class="nav-item active">
             <a class="nav-link" href="/boiteidee">Boite à idée <span class="sr-only">(current)</span></a>
         </li>
-		@if(\Request::is('boutique')){
+		@if(\Request::is('boutique'))
 		<li>
             <form class="form-inline my-2 my-md-0">
                 <input class="form-control" type="text" placeholder="Recherche">
